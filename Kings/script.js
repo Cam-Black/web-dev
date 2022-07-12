bcd67af3702e4495d2a66d3d25d4f702f803b2f2
cam-black <cameronablack1998@gmail.com>
2022-07-12 15:26:11 +0100

    Created web page to render Kings and search by King name.

---
+++ Kings/script.js
@@ -0,0 +1,58 @@
+"use strict"
+
+const output = document.querySelector('#output');
+const searchBtn = document.querySelector('#search');
+const inputKings = document.querySelector('#searchKings');
+const outputKings = document.querySelector('#searchResults');
+const reset = document.querySelector('#reset');
+
+axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json")
+	.then(response => {
+		for (let i = 0; i < response.data.length; i++) {
+			const kings = document.createElement('p');
+			kings.innerText = `Name: ${response.data[i].nm}
+                City: ${response.data[i].cty}
+                House: ${response.data[i].hse}
+                Years: ${response.data[i].yrs}`;
+			
+			output.appendChild(kings);
+		}
+	})
+	.catch(error => console.error(error));
+
+const findByName = event => {
+	event.preventDefault();
+	while (outputKings.firstChild) {
+		outputKings.removeChild(outputKings.lastChild);
+	}
+	let input = inputKings.value;
+	input = input.toLowerCase();
+	
+	axios.get("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json")
+		.then(response => {
+			const results = response.data.filter(king => king.nm.toLowerCase().startsWith(input.toLowerCase()));
+			output.style.display = "none";
+			
+			for (let i = 0; i < results.length; i++) {
+				const kings = document.createElement('p');
+				kings.innerText = `Name: ${results[i].nm}
+                City: ${results[i].cty}
+                House: ${results[i].hse}
+                Years: ${results[i].yrs}`;
+				outputKings.appendChild(kings);
+			}
+		})
+		.catch(error => console.error(error));
+}
+
+const clearResults = event => {
+	event.preventDefault();
+	output.style.display = "block";
+	while (outputKings.firstChild) {
+		outputKings.removeChild(outputKings.lastChild);
+	}
+	inputKings.value = '';
+}
+
+searchBtn.addEventListener('click', findByName);
+reset.addEventListener('click', clearResults);
